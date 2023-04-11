58aa350bda242dde513b496f0f7b2a7913f2ca1e
Jan Polacek <xpolace5@fi.muni.cz>
2023-04-11 12:56:47 +0200

    Added new tests

--- hw03/tests/tests-mine.c
+++ hw03/tests/tests-mine.c
@@ -16,46 +16,108 @@
  * • ‹app_main_args(ARG…)› — call your ‹main()› with given arguments.
  * • ‹app_main()› — call your ‹main()› without any arguments. */
 
-// TEST(my_test_1)
-// {
-//     int rv = 0; /* return value of main()*/
-//     CHECK(app_main_args("-s" "data/Brno-BosonohyContainers.csv" "data/Brno-BosonohyPaths.csv") == rv);
+TEST(my_test_1)
+{
+    int rv = 0; /* return value of main()*/
+    CHECK(app_main_args("-t" "APBGC" "tests/data/example-containers.csv" "tests/data/example-paths.csv") == rv);
 
-//     /* TIP: Use ‹app_main()› to test the program without arguments. */
+    /* TIP: Use ‹app_main()› to test the program without arguments. */
 
-//     const char *correct_output =
-//             "1;APBGCT;2,3,4,5,6,7,8,9,10,11\n"
-//             "2;APGC;1,3,4,5,6,7,8,9,10,11\n"
-//             "3;AGCT;1,2,5,7,8,9,10\n"
-//             "4;APBGC;1,2,6,7,8,11\n"
-//             "5;APBGC;1,2,3,8,9,10\n"
-//             "6;APBGCT;1,2,4,7,11\n"
-//             "7;APBGC;1,2,3,4,6,8,11\n"
-//             "8;APBGC;1,2,3,4,5,7\n"
-//             "9;APGC;1,2,3,5,10\n"
-//             "10;APGCT;1,2,3,5,9\n"
-//             "11;B;1,2,4,6,7"
-//             ;
+    const char *correct_output =
+            "ID: 1, Type: Colored glass, Capacity: 1550, Address: Drozdi 55, Neighbors: 4\n"
+            "ID: 2, Type: Clear glass, Capacity: 1550, Address: Drozdi 55, Neighbors: 4\n"
+            "ID: 3, Type: Plastics and Aluminium, Capacity: 1100, Address: Drozdi 55, Neighbors: 4\n"
+            "ID: 4, Type: Colored glass, Capacity: 900, Address: Drozdi 55, Neighbors: 1 2 3 5 8\n"
+            "ID: 5, Type: Paper, Capacity: 5000, Address: Klimesova 60, Neighbors: 4 8\n"
+            "ID: 6, Type: Colored glass, Capacity: 3000, Address: Klimesova 60, Neighbors: 8\n"
+            "ID: 7, Type: Plastics and Aluminium, Capacity: 5000, Address: Klimesova 60, Neighbors: 8\n"
+            "ID: 8, Type: Biodegradable waste, Capacity: 3000, Address: Na Buble 5, Neighbors: 4 5 6 7 11\n"
+            "ID: 10, Type: Plastics and Aluminium, Capacity: 900, Address: Odlehla 70, Neighbors: 9\n"
+            "ID: 11, Type: Paper, Capacity: 2000, Address: Odlehla 70, Neighbors: 8\n"
+            ;
 
-//     ASSERT_FILE(stdout, correct_output);
-//     CHECK_IS_EMPTY(stderr);
-// }
+    ASSERT_FILE(stdout, correct_output);
+    CHECK_IS_EMPTY(stderr);
+}
 
-// TEST(my_test_2)
-// {
-//     int rv = 0; /* return value of main()*/
-//     CHECK(app_main_args("-t" "BT" "-c" "500-2000" "-p" "Y" "data/Brno-BosonohyContainers.csv" "data/Brno-BosonohyPaths.csv") == rv);
+TEST(my_test_2)
+{
+    int rv = 0; /* return value of main()*/
+    CHECK(app_main_args("-c" "5000-5000" "tests/data/example-containers.csv" "tests/data/example-paths.csv") == rv);
 
-//     const char *correct_output =
-//             "ID: 22164, Type: Textile, Capacity: 1000, Address: Bosonozske namesti 669, Neighbors: 4135 4477 22119 22698 23952\n"
-//             "ID: 22119, Type: Textile, Capacity: 1000, Address: Prazska 702, Neighbors: 4135 4136 4481 4482 6198 6199 6297 6298 6719 6720 6723 6724 8300 8301 10440 10660 10879 10882 10897 10974 11119 11612 11613 11844 12090 12094 12095 12096 12109 12189 12346 13657 14376 17490 18509 22164 22212 22698 23200 23201 23947 23948 23949 23951 23952 23953 23954 24045 24048 24053 24054 24055 26115 26242\n"
-//             "ID: 22211, Type: Textile, Capacity: 1000, Address: K Berce 721, Neighbors: 4477 22698 23947 23952 24049\n"
-//             "ID: 22212, Type: Textile, Capacity: 1000, Address: Pracata 783, Neighbors: 4135 13657 22119 23947 23952"
-//     ;
+    const char *correct_output =
+            "ID: 5, Type: Paper, Capacity: 5000, Address: Klimesova 60, Neighbors: 4 8\n"
+            "ID: 7, Type: Plastics and Aluminium, Capacity: 5000, Address: Klimesova 60, Neighbors: 8\n"
+    ;
 
-//     ASSERT_FILE(stdout, correct_output);
-//     CHECK_IS_EMPTY(stderr);
-// }
+    ASSERT_FILE(stdout, correct_output);
+    CHECK_IS_EMPTY(stderr);
+}
+
+TEST(my_test_3)
+{
+    int rv = 0; /* return value of main()*/
+    CHECK(app_main_args("-t" "APBGC" "-c" "500-1600" "tests/data/example-containers.csv" "tests/data/example-paths.csv") == rv);
+
+    const char *correct_output =
+            "ID: 1, Type: Colored glass, Capacity: 1550, Address: Drozdi 55, Neighbors: 4\n"
+            "ID: 2, Type: Clear glass, Capacity: 1550, Address: Drozdi 55, Neighbors: 4\n"
+            "ID: 3, Type: Plastics and Aluminium, Capacity: 1100, Address: Drozdi 55, Neighbors: 4\n"
+            "ID: 4, Type: Colored glass, Capacity: 900, Address: Drozdi 55, Neighbors: 1 2 3 5 8\n"
+            "ID: 10, Type: Plastics and Aluminium, Capacity: 900, Address: Odlehla 70, Neighbors: 9\n"
+    ;
+
+    ASSERT_FILE(stdout, correct_output);
+    CHECK_IS_EMPTY(stderr);
+}
+
+TEST(my_test_4)
+{
+    int rv = 0; /* return value of main()*/
+    CHECK(app_main_args("-p" "Y" "tests/data/example-containers.csv" "tests/data/example-paths.csv") == rv);
+
+    const char *correct_output =
+            "ID: 1, Type: Colored glass, Capacity: 1550, Address: Drozdi 55, Neighbors: 4\n"
+            "ID: 2, Type: Clear glass, Capacity: 1550, Address: Drozdi 55, Neighbors: 4\n"
+            "ID: 3, Type: Plastics and Aluminium, Capacity: 1100, Address: Drozdi 55, Neighbors: 4\n"
+            "ID: 4, Type: Colored glass, Capacity: 900, Address: Drozdi 55, Neighbors: 1 2 3 5 8\n"
+            "ID: 8, Type: Biodegradable waste, Capacity: 3000, Address: Na Buble 5, Neighbors: 4 5 6 7 11\n"
+            "ID: 9, Type: Textile, Capacity: 500, Address: Na Buble 5, Neighbors: 10\n"
+            "ID: 10, Type: Plastics and Aluminium, Capacity: 900, Address: Odlehla 70, Neighbors: 9\n"
+            "ID: 11, Type: Paper, Capacity: 2000, Address: Odlehla 70, Neighbors: 8\n"
+    ;
+
+    ASSERT_FILE(stdout, correct_output);
+    CHECK_IS_EMPTY(stderr);
+}
+
+TEST(my_test_5)
+{
+    int rv = 0; /* return value of main()*/
+    CHECK(app_main_args("-p" "Y" "tests/data/example-containers.csv" "tests/data/example-paths.csv") == rv);
+
+    const char *correct_output =
+            "ID: 5, Type: Paper, Capacity: 5000, Address: Klimesova 60, Neighbors: 4 8\n"
+            "ID: 6, Type: Colored glass, Capacity: 3000, Address: Klimesova 60, Neighbors: 8\n"
+            "ID: 7, Type: Plastics and Aluminium, Capacity: 5000, Address: Klimesova 60, Neighbors: 8\n"
+    ;
+
+    ASSERT_FILE(stdout, correct_output);
+    CHECK_IS_EMPTY(stderr);
+}
+
+TEST(my_test_6)
+{
+    int rv = 0; /* return value of main()*/
+    CHECK(app_main_args("-t" "C" "-p" "Y" "tests/data/example-containers.csv" "tests/data/example-paths.csv") == rv);
+
+    const char *correct_output =
+            "ID: 6, Type: Colored glass, Capacity: 3000, Address: Klimesova 60, Neighbors: 8\n"
+    ;
+
+    ASSERT_FILE(stdout, correct_output);
+    CHECK_IS_EMPTY(stderr);
+}
 
 TEST(my_test_empty)
 {
